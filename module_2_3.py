0025eeea2d56547a983cd92f4e3cc53351659245
tagir2211 <tagir2211@gmail.com>
2024-08-03 23:14:12 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,13 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+i = 0
+while i <= len(my_list):
+    if 0 < my_list[i]:
+        print(my_list[i])
+    else:
+        if my_list[i] == 0:
+            i = i + 1
+            continue
+        break
+    i = i + 1
+
+
